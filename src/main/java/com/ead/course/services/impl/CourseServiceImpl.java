5ad93d3016a4f20a234b6194b9751835f724571d
Matheus <matheus.sl.dh@gmail.com>
2022-06-21 08:55:49 -0300

    feat: implements notification command with new user Subscription

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,14 +1,18 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.dtos.NotificationCommandDto;
 import com.ead.course.model.CourseModel;
 import com.ead.course.model.LessonModel;
 import com.ead.course.model.ModuleModel;
+import com.ead.course.model.UserModel;
+import com.ead.course.publishers.NotificationCommandPublisher;
 import com.ead.course.repositories.CourseRepository;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.CourseService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -20,6 +24,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class CourseServiceImpl implements CourseService {
 
@@ -27,6 +32,7 @@ public class CourseServiceImpl implements CourseService {
     private final ModuleRepository moduleRepository;
     private final LessonRepository lessonRepository;
     private final UserRepository userRepository;
+    private final NotificationCommandPublisher notificationCommandPublisher;
 
     @Override
     public CourseModel save(CourseModel courseModel) {
@@ -52,6 +58,21 @@ public class CourseServiceImpl implements CourseService {
     @Transactional
     public void saveSubscriptionUserInCourse(UUID courseId, UUID userId) {
         courseRepository.saveCourseUser(courseId, userId);
+    }
+
+    @Override
+    @Transactional
+    public void saveSubscriptionUserInCourseAndSendNotification(CourseModel course, UserModel user) {
+        courseRepository.saveCourseUser(course.getCourseId(), user.getUserId());
+        try {
+            var notificationCommandDto = new NotificationCommandDto();
+            notificationCommandDto.setTitle("Bem-Vindo(a) ao Curso: " + course.getName());
+            notificationCommandDto.setMessage(user.getFullName() + " a sua inscrição foi realizada com sucesso!");
+            notificationCommandDto.setUserId(user.getUserId());
+            notificationCommandPublisher.publishNotificationCommand(notificationCommandDto);
+        } catch (Exception e) {
+            log.warn("Error sending notification!");
+        }
     }
 
     @Override
